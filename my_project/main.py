356758d12e695e59e8b2c1049ea4f7314af46da2
ZheniaKim <zhenia.kim3597@gmail.com>
2025-07-18 14:21:22 +0900

    creat_user in 'DB'

--- my_project/main.py
+++ my_project/main.py
@@ -1,9 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+
+
 
 app = FastAPI()
 
+Users = []
+Posts = []
+
 class User(BaseModel):
-    id:int
     username:str
+    posts : list = []
+    friend:list = []
+
+
+class Post(BaseModel):
+    author:User
+    text:str    
     
+
+@app.get('/main')
+def get_posts()
+    return Posts
+
+
+@app.create_user('/creat_user')
+def create_user(username:str):
+    for user in Users:
+        if user.username == username:
+            raise HTTPException(status_code=409, detail='Username is in Database')
+        else:
+            new_user = User(username=username)
+            Users.append(new_user)
+    return {'message': 'username was created'}        
+
+
